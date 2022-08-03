894290adb093f6d527752574e7f35fbd1f395f0d
katya <kotenokches@rambler.ru>
2022-08-03 17:35:04 +0500

    Первый коммит

---
+++ src/main/java/pro/DolgovaEkaterinaBot/DolgovaEkaterinaBotApplication.java
@@ -0,0 +1,13 @@
+package skypro.DolgovaEkaterinaBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolgovaEkaterinaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolgovaEkaterinaBotApplication.class, args);
+	}
+
+}
